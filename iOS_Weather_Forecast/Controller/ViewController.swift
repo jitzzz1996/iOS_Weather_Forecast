a3f133c6007bbf546d11e429dceeac395a89a72d
Jitesh <jiteshbablani1996@gmail.com>
2023-06-18 12:38:58 +0530

    API Data integration

--- iOS_Weather_Forecast/Controller/ViewController.swift
+++ iOS_Weather_Forecast/Controller/ViewController.swift
@@ -13,15 +13,44 @@ class ViewController: UIViewController {
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
+    
+    let weatherManager = WeatherManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.baseSetup()
         // Do any additional setup after loading the view.
     }
+    
+    private func baseSetup() {
+        self.searchTextField.delegate = self
+    }
+
     @IBAction func searchButtonAction(_ sender: UIButton) {
-        print(searchTextField.text)
+        searchTextField.endEditing(true)
     }
     
 
+}
+extension ViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        searchTextField.endEditing(true)
+        return true
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField.text != "" {
+            return true
+        } else {
+            textField.placeholder = "Please type something"
+            return false 
+        }
+    }
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if let city = textField.text {
+            weatherManager.fetchWeather(city: city)
+        }
+        searchTextField.text = ""
+    }
 }
 
